5916baa5898890830b305be4393b991f9f8b2f60
bouzanizied0-afk <bouzanizied0@gmail.com>
2026-02-11 00:06:44 +0100

    Update serverCore.js

--- serverCore.js
+++ serverCore.js
@@ -4,12 +4,15 @@ import { getDatabase, ref, set, onValue, get } from "https://www.gstatic.com/fir
 
 /* ========= إعداد الخادم ========= */
 export const firebaseConfig = {
-  apiKey: "ضع_API_KEY_هنا",
+  apiKey: "AIzaSyC_oqjbxC27qLhH6mx_ngg4WCZUrMW2B7U",
   authDomain: "zied-e3b78.firebaseapp.com",
-  databaseURL: "https://zied-e3b78-default-rtdb.europe-west1.firebasedatabase.app/",
-  projectId: "zied-e3b78"
+  databaseURL: "https://zied-e3b78-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "zied-e3b78",
+  storageBucket: "zied-e3b78.firebasestorage.app",
+  messagingSenderId: "774724725374",
+  appId: "1:774724725374:web:a338e16e8689d60dd3062f",
+  measurementId: "G-20CPRZJ5JE"
 };
-
 export const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
 
